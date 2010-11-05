33294d7f0e05df7b7b3c3bb8ad29b47856cdaf7a
John SJ Anderson <genehack@genehack.org>
2010-11-04 21:12:17 -0400

    initial version

--- lib/Dist/Zilla/PluginBundle/GENEHACK.pm
+++ lib/Dist/Zilla/PluginBundle/GENEHACK.pm
@@ -1,5 +1,63 @@
-use strict;
-use warnings;
-package dist::zilla::pluginbundle::genehack;
+package Dist::Zilla::PluginBundle::GENEHACK;
+
+# ABSTRACT: BeLike::GENEHACK when you zilla your dist
+
+=head1 SYNOPSIS
+
+Loads the stuff I find myself using all the time. Currently equivalent to
+this:
+
+    [@Git]
+    [Git::NextVersion]
+    first_version = 0.01
+
+    [@Basic]
+
+    [AutoPrereqs]
+    [CriticTests]
+    [ExtraTests]
+    [PodCoverageTests]
+    [PodSyntaxTests]
+    [PodWeaver]
+
+=cut
+
+use Moose;
+use namespace::autoclean;
+use Dist::Zilla;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+BEGIN {
+  $Dist::Zilla::PluginBundle::GENEHACK::VERSION = '1.004';
+}
+
+use Dist::Zilla::PluginBundle::Basic;
+use Dist::Zilla::PluginBundle::Git;
+
+sub configure {
+  my $self = shift;
+
+  $self->add_bundle('Git');
+  $self->add_plugins(
+    [ 'Git::NextVersion' => { first_version => '0.01 '} ],
+  );
+
+  $self->add_bundle('Basic');
+  $self->add_plugins(
+    'AutoPrereqs' ,
+    'CriticTests' ,
+    'ExtraTests' ,
+    'MetaConfig' ,
+    'MetaJSON' ,
+    'PodCoverageTests' ,
+    'PodSyntaxTests' ,
+    'PodWeaver' ,
+    'Repository' ,
+  )
+}
 
 1;
+
+=for Pod::Coverage configure
+
+=cut
